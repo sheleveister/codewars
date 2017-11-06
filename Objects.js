fcc71a9885f318d6cf5a9eab7166461b24a7dccd
sheleveister <e.sheleveister@gmail.com>
2017-11-06 23:43:06 +0300

    Objects

--- Objects.js
+++ Objects.js
@@ -29,7 +29,7 @@ function isEmpty(obj) {
 
 let shedule = {};
 
-console.log (isEmpty (shedule));
+console.log (isEmpty(shedule));
 
 shedule['8:30'] = 'up';
 
@@ -48,7 +48,7 @@ let getSalaries = (obj) => {
   }
   return sum;
 };
-console.log (getSalaries (salaries));
+console.log (getSalaries(salaries));
 
 
 let maxCount = (salaries) => {
@@ -58,7 +58,7 @@ let maxCount = (salaries) => {
   }
   return b;
 };
-console.log (maxCount (salaries));
+console.log (maxCount(salaries));
 
 let cars = {};
 
@@ -69,11 +69,11 @@ function isCars(cars) {
   return 'no properties';
 }
 
-console.log (isCars (cars));
+console.log (isCars(cars));
 
 cars['bmw'] = 'X2';
 
-console.log (isCars (cars));
+console.log (isCars(cars));
 
 let menu = {
   width: 200,
@@ -95,8 +95,9 @@ function multipleNumeric(obj) {
 
 }
 
-multipleNumeric (menu);
-console.log (menu);
+multipleNumeric(menu);
+console.log(menu);
+
 
 let browsers = {
   Chrome: 65,
@@ -108,3 +109,140 @@ let lastVersions = browsers;
 browsers.Firefox = 58;
 
 console.log(lastVersions);
+
+
+let user = {
+
+  name: 'Luke',
+  say: function() {
+    return this.name + ', I am your father'
+  }
+
+};
+
+let say = user.say();
+
+
+let objSubordinates = {
+  title: 'Rachel',
+  type: 'Closed',
+  img: 'c9a66e5e26c4.png',
+  subordinates: [
+    {
+      title: 'Vincent',
+      img: 'c9a66e5e26c4.png',
+      office: 'West',
+      subordinates: [],
+    },
+    {
+      title: 'Robert',
+      position: {
+        name: 'business management',
+      },
+      office: 'East',
+      subordinates: [
+        {
+          title: 'Ann',
+          position: {
+            name: 'Director',
+          },
+          office: 'East',
+          type: 'Closed',
+          img: 'a66e5e26c4.png',
+        },
+      ]
+    },
+  ]
+};
+
+function setObjWithChildProp(obj) {
+
+  for (let key in obj) {
+
+    if (key === 'subordinates') {
+      obj.children = obj[key];
+      delete obj.subordinates;
+
+      let objSub = obj.children;
+
+      getObjSub(objSub);
+      return obj;
+    }
+
+  }
+
+}
+
+function getObjSub(objSub) {
+
+  for (let i = 0; i < objSub.length; i++) {
+
+    if ((objSub[i]).subordinates.length) {
+      objSub[i].children = (objSub[i]).subordinates;
+      delete (objSub[i]).subordinates;
+      return objSub;
+    }
+    delete (objSub[i]).subordinates;
+
+  }
+
+}
+
+let objWithChild = setObjWithChildProp(objSubordinates);
+
+
+function User(firstName, lastName) {
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+
+  let phrase = 'Hi';
+
+  function getFullName() {
+    return `${firstName} ${lastName}`;
+  }
+
+  this.sayHi = function() {
+    return phrase + ', ' + getFullName();
+  }
+
+}
+
+let robert = new User('Robert', 'Rabkin');
+console.log(robert.sayHi());
+
+
+let user = {
+  name: "Nick",
+  age: 30
+};
+
+let keys = Object.keys(user);
+console.log(keys);
+
+
+function Visitor() {
+}
+
+let guest = new Visitor();
+
+
+let visitor = {
+  name: 'Ragnar',
+  lastName: 'Danneshild',
+
+  get fullName() {
+    return `${this.name} ${this.lastName}`;
+  },
+
+  set fullName(value) {
+    let fullName = value.split(' ');
+    this.name = fullName[0];
+    this.lastName = fullName[1];
+    return `${this.name} ${this.lastName}`;
+  }
+
+};
+
+console.log(visitor.fullName);
+console.log(visitor.fullName = 'Van Gog');
